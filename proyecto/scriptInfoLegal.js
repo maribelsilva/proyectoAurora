e5e217f35fa97a4d16305c7a7a590530b10e89cf
maribelsilva <maribelalejandrasilvao@gmail.com>
2023-10-02 21:41:39 -0300

    modifiqué formulario html e hice js del formulario

--- proyecto/scriptInfoLegal.js
+++ proyecto/scriptInfoLegal.js
@@ -1,36 +1,134 @@
 //guardarLocalStorage();
 
-obtenerLocalStorage()
+// obtenerLocalStorage()
 
-    function obtenerLocalStorage(){
+//     function obtenerLocalStorage(){
 
-    if(localStorage.getItem("persona")){
+//     if(localStorage.getItem("persona")){
 
-        // existe entrada persona en el localStorage 
-        let nombre = localStorage.getItem("nombre");
-        let persona = JSON.parse(localStorage.getItem("persona"));
+//         // existe entrada persona en el localStorage 
+//         let nombre = localStorage.getItem("nombre");
+//         let persona = JSON.parse(localStorage.getItem("persona"));
 
-        console.log(nombre);
-        console.log(persona);
+//         console.log(nombre);
+//         console.log(persona);
 
-      }else{
-        console.log("No hay entradas en el localStorage")
-        //para que no me tire null
+//       }else{
+//         console.log("No hay entradas en el localStorage")
+//         //para que no me tire null
+//       }
+
+//     }
+
+
+// function guardarLocalStorage() {
+//     let persona = {
+//         nombre: "Ana",
+//         email: "ana@gmail.com",
+//         mensaje: "Quiero adoptar un perrito geronte"
+//     };
+
+//     let nombre = "Bruno";
+
+
+
+// localStorage.setItem("persona", JSON.stringify(persona));
+// }
+
+
+
+window.onload = () => {
+    let formulario = document.querySelector("form");
+    let inputNombre = document.getElementById("name");
+    let inputEmail = document.getElementById("email");
+    let inputPass = document.getElementById("pass");
+    let inputRepeatPass = document.getElementById("repeatPass");
+    let boton = document.getElementById("boton");
+
+    //mensaje de error si el pass no cumple con ciertos requisitos
+    inputPass.addEventListener("keyup", () => {
+        let mensaje;
+        if (inputPass.value.length < 8) {
+            mensaje = "La contrasenia tiene que tener al menos 8 caracteres"
+            document.getElementById("mensajePass").innerText = mensaje;
+        } else {
+            document.getElementById("mensajePass").innerText = " "
+        }
+    })
+
+    //cheque que sean iguales dos campos
+    inputRepeatPass.addEventListener("keyup", () => {
+        let mensaje;
+        if (inputRepeatPass.value != inputPass.value) {
+            mensaje = "Las contrasenias deben coincidir."
+            document.getElementById("mensajeRepeatPass").innerText = mensaje
+        } else {
+            document.getElementById("mensajeRepeatPass").innerText = " "
+        }
+    })
+
+    //borde rojo al quitar el cursor de un input cuando esta vacio
+    
+    inputNombre.addEventListener("blur", () => {
+      if (inputNombre.value == "") {
+        inputNombre.style.border = "2px solid red"
+      } else {
+        inputNombre.style.border = "0"
       }
+  })
 
+  inputNombre.addEventListener("keyup", () => {
+    let mensaje;
+    if (inputNombre.value.length < 4) {
+        mensaje = "El nombre debe contener más de tres letras."
+        document.getElementById("mensajeNombre").innerText = mensaje;
+    } else {
+        document.getElementById("mensajeNombre").innerText = " "
     }
+})
+  
+    inputEmail.addEventListener("blur", () => {
+      if (inputEmail.value == "") {
+      inputEmail.style.border = "2px solid red"
+      } else {
+      inputEmail.style.border = "0"
+      }
+  })
 
 
-function guardarLocalStorage() {
-    let persona = {
-        nombre: "Ana",
-        email: "ana@gmail.com",
-        mensaje: "Quiero adoptar un perrito geronte"
-    };
+    inputRepeatPass.addEventListener("blur", () => {
+      if (inputRepeatPass.value == "") {
+      inputRepeatPass.style.border = "2px solid red"
+      } else {
+      inputRepeatPass.style.border = "0"
+      }
+    
+    })
 
-    let nombre = "Bruno";
+    
+    inputPass.addEventListener("blur", () => {
+      if (inputPass.value == "") {
+      inputPass.style.border = "2px solid red"
+      } else {
+      inputPass.style.border = "0"
+      }
+  })
+  
+        
+    //boton deshabilitado si no completo el form
+    formulario.addEventListener("change", (event) => {
+        event.preventDefault()
+        if (inputNombre.value.trim() != "" && inputPass.value.trim() != "" && inputRepeatPass.value.trim() != "") {
+            boton.removeAttribute("disabled");
+        } else {
+            boton.setAttribute("disabled", "disabled");
+            
+        }
+            
+    })
 
 
 
-localStorage.setItem("persona", JSON.stringify(persona));
+
+
 }
